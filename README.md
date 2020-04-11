bc66811f1143f3dfb14c55af397c169f7a369955
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-04-11 01:06:54 -0300

    Update ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-![Netlify Status](https://api.netlify.com/api/v1/badges/6c542871-5683-4168-a5d2-548833ada782/deploy-status)
-
 ![ReactJS](https://img.shields.io/static/v1?label=Frontend&message=ReactJS&color=blue&style=for-the-badge&logo=React)
+![](https://img.shields.io/github/languages/top/rodcordeiro/myPage.svg?color=yellow&style=for-the-badge)
 
-[RodCordeiro](https://rodcordeiro.com.br/)
+
+![](https://img.shields.io/github/last-commit/rodcordeiro/myPage.svg?color=red&style=for-the-badge)
+![](https://img.shields.io/github/languages/count/rodcordeiro/myPage.svg?color=yellow&style=for-the-badge)
+![](https://img.shields.io/github/repo-size/rodcordeiro/myPage.svg?color=blueviolet&style=for-the-badge)
+
+
+![Website Status](https://img.shields.io/website?down_color=orange&down_message=Offline&style=for-the-badge&up_color=green&up_message=Online&url=http%3A%2F%2Frodcordeiro.com.br%2F)
+![Netlify Status](https://api.netlify.com/api/v1/badges/6c542871-5683-4168-a5d2-548833ada782/deploy-status)
+![Security Headers](https://img.shields.io/security-headers?style=for-the-badge&url=https%3A%2F%2Frodcordeiro.com.br%2F)
+
+[https://rodcordeiro.com.br/](https://rodcordeiro.com.br/)
+
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
 import { FiHome } from 'react-icons/fi';
+import {  FaWhatsapp, FaMailBulk, FaLinkedinIn, FaTelegramPlane, FaDiscord, FaGithub, FaSkype, FaAddressCard } from 'react-icons/fa';
+
 import './style.css';
 
 export default function Contact(){
     return (
         <div className="contactContent">
-           <div className="contact">
+           <div className="contacts">
                <header>
                     <h1>Contato</h1>
                     <Link 
@@ -18,10 +21,49 @@ export default function Contact(){
                     </Link>
                </header>
                <hr />
-               <br />
-               <p> Ainda em desenvolvimento, mas qualquer coisa manda e-mail para <a href="mailto:rodrigomendoncca@gmail.com?subject=Sobre%20seu%20portfolio">rodrigomendoncca@gmail.com</a></p>
+               <div className="contact-data">
+               
+                    <div className="contact"> 
+                       <a href="mailto:rodrigomendoncca@gmail.com">
+                           <FaMailBulk /> rodrigomendoncca@gmail.com
+                        </a>
+                    </div>
+                    
+                    <div className="contact"> <FaWhatsapp /> (11) 9 6918-7148 </div>
+                    
+                    <div className="contact"><FaTelegramPlane /> 
+                        @RodCordeiro
+                    </div>
+                    
+                    <div className="contact">
+                        <FaDiscord /> @RodCordeiro#2122 
+                    </div>
+                    
+                    <div className="contact"> <FaSkype /> RodrigoM.Cordeiro </div>
+                    
+                    <div className="contact"> 
+                        <a href="https://www.linkedin.com/in/rodrigomcordeiro/">
+                            <FaLinkedinIn /> /rodrigomcordeiro
+                        </a>
+                    </div>
+                    
+                    <div className="contact">
+                        <a href="https://github.com/rodcordeiro">
+                            <FaGithub /> /rodcordeiro
+                        </a>
+                    </div>
 
- 
+                    <div className="contact">
+                         <a 
+                         href="https://rodcordeiro.github.io/shares/files/rodrigo_cordeiro.vcf" 
+                         download="https://rodcordeiro.github.io/shares/files/rodrigo_cordeiro.vcf"
+                         >
+                            <FaAddressCard /> Baixe o .vcard
+                        </a>
+                    </div>
+                    
+
+               </div>
             </div>
         </div>
     );
