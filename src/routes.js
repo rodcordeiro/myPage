63b6cff090012f023666d914ee8ac03339bdbf3c
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-09-06 19:23:21 -0300

    Add login route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import MainPage from './pages/Main';
+import LoginPage from './pages/Login';
 
 
 export default function Routes() {
@@ -9,6 +10,7 @@ export default function Routes() {
         <BrowserRouter>
         <Switch>
             <Route path="/" exact component={MainPage}/>
+            <Route path="/login"  component={LoginPage}/>
         </Switch>
         </BrowserRouter>
     );
